f45c61074755fe3730ddcb55a7ca1c8153ad6f9d
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-06-14 12:35:41 +0200

    feat: write keydown docs

--- lib/src/key_down_event_listener.dart
+++ lib/src/key_down_event_listener.dart
@@ -5,17 +5,19 @@ import 'dart:io';
 import 'package:collection/collection.dart';
 import 'package:commander_ui/src/application/stdin_buffer.dart';
 import 'package:commander_ui/src/commons/ansi_character.dart';
+import 'package:commander_ui/src/key_down_listener.dart';
 
+/// Listens to key down events and executes the corresponding callbacks.
 class KeyDownEventListener {
   StreamSubscription? sigintSubscription;
   late StreamSubscription<String>? subscription;
   final List<KeyDownListener> listeners = [];
   FutureOr<void> Function(String, void Function())? fallback;
 
+  /// Creates a new instance of [KeyDownEventListener] and starts listening to key down events.
   KeyDownEventListener() {
     subscription = StdinBuffer.stream.transform(utf8.decoder).listen((data) {
-      final listener =
-          listeners.firstWhereOrNull((listener) => listener.key.value == data);
+      final listener = listeners.firstWhereOrNull((listener) => listener.key.value == data);
       if (listener case KeyDownListener listener) {
         listener.callback(data, dispose);
         return;
@@ -27,22 +29,25 @@ class KeyDownEventListener {
     });
   }
 
-  void match(AnsiCharacter key,
-      void Function(String, void Function() dispose) callback) {
+  /// Adds a new listener for the specified [key].
+  /// When the [key] is pressed, the [callback] is executed.
+  void match(AnsiCharacter key, void Function(String, void Function() dispose) callback) {
     listeners.add(KeyDownListener(key, callback));
   }
 
-  void catchAll(
-      FutureOr<void> Function(String, void Function() dispose) callback) {
+  /// Sets a fallback listener that is called when a key is pressed that does not have a specific listener.
+  void catchAll(FutureOr<void> Function(String, void Function() dispose) callback) {
     fallback = callback;
   }
 
+  /// Sets a listener that is called when the application is about to exit.
   onExit(Function(void Function() dispose) callback) {
     sigintSubscription = ProcessSignal.sigint.watch().listen((event) {
       callback(dispose);
     });
   }
 
+  /// Disposes the listener, stopping it from listening to key down events.
   void dispose() {
     subscription?.cancel();
     subscription = null;
@@ -50,11 +55,4 @@ class KeyDownEventListener {
     sigintSubscription?.cancel();
     sigintSubscription = null;
   }
-}
-
-final class KeyDownListener {
-  final AnsiCharacter key;
-  final void Function(String, void Function()) callback;
-
-  KeyDownListener(this.key, this.callback);
 }
