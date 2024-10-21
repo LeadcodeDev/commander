f653cca26d0f822d0be18fe7f4c3dfcc1accfedf
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-10-21 11:24:47 +0200

    refactor: remove old project files

--- lib/src/component.dart
+++
@@ -1,4 +0,0 @@
-/// An abstract interface for a component.
-/// A component is a part of a system that has a specific function and can operate independently.
-/// This interface defines a contract for components that can handle certain tasks and produce a result of type [T].
-abstract interface class Component<T> {}
