960f7d095e3cbb810a4a250a7e477e750e736bc1
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-10-22 00:23:34 +0200

    feat(screen): implement screen component

---
+++ lib/src/application/components/screen.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+import 'package:commander_ui/src/domains/models/component.dart';
+import 'package:mansion/mansion.dart';
+
+final class Screen implements Component<ScreenManager> {
+  String? _title;
+
+  Screen({String? title}) {
+    _title = title;
+  }
+
+  @override
+  ScreenManager handle() => ScreenManager(this);
+}
+
+final class ScreenManager {
+  final Screen _screen;
+
+  ScreenManager(this._screen);
+
+  void setTitle(String title) {
+    _screen._title = title;
+    stdout.writeAnsi(SetTitle(title));
+  }
+
+  void enter() {
+    stdout.writeAnsi(AlternateScreen.enter);
+    if (_screen._title case String title) {
+      stdout.writeAnsi(SetTitle(title));
+    }
+  }
+
+  void leave() {
+    stdout.writeAnsi(AlternateScreen.leave);
+  }
+}
