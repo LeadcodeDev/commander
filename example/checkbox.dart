ffb6e2d6620b5c93056c699b8d3922b32609eba1
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-10-21 13:13:46 +0200

    feat(checkbox): implement checkbox component

---
+++ example/checkbox.dart
@@ -0,0 +1,16 @@
+import 'package:commander_ui/src/commander.dart';
+import 'package:commander_ui/src/level.dart';
+
+enum Shape { square, circle, triangle }
+
+Future<void> main() async {
+  final commander = Commander(level: Level.verbose);
+  print('Hello World !');
+
+  final value = await commander.checkbox('What is your name ?',
+      defaultValue: 'Charlie',
+      options: ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank', 'John']
+  );
+
+  print(value);
+}
---
+++ lib/src/application/components/checkbox.dart
@@ -0,0 +1,151 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:commander_ui/src/application/terminals/terminal.dart';
+import 'package:commander_ui/src/application/utils/terminal_tools.dart';
+import 'package:commander_ui/src/domains/models/component.dart';
+import 'package:commander_ui/src/io.dart';
+import 'package:mansion/mansion.dart';
+
+final class Checkbox<T> with TerminalTools implements Component<List<T>> {
+  final _completer = Completer<List<T>>();
+
+  final Terminal _terminal;
+
+  int _currentIndex = 0;
+
+  late final String _message;
+  late final T? _defaultValue;
+  late final String _placeholder;
+  late final String Function(T)? _onDisplay;
+  late final List<T> _options;
+  final List<int> _selectedOptions = [];
+
+  bool _keepAlive = true;
+
+  Checkbox(this._terminal,
+      {required String message,
+      required List<T> options,
+      T? defaultValue,
+      String placeholder = '',
+      String Function(T)? onDisplay}) {
+    _message = message;
+    _options = options;
+    _defaultValue = defaultValue;
+    _placeholder = placeholder;
+    _onDisplay = onDisplay;
+
+    if (_defaultValue case T value) {
+      _currentIndex = _options.indexOf(value);
+    }
+  }
+
+  @override
+  Future<List<T>> handle() async {
+    saveCursorPosition();
+    hideCursor();
+
+    _render(isInitialRender: true);
+
+    while (_keepAlive) {
+      final key = readKey(_terminal);
+
+      if (key.controlChar == ControlCharacter.arrowUp || key.char == 'k') {
+        if (_currentIndex != 0) {
+          _currentIndex = _currentIndex - 1;
+          _render();
+        }
+      } else if (key.controlChar == ControlCharacter.arrowDown || key.char == 'j') {
+        if (_currentIndex < _options.length - 1) {
+          _currentIndex = _currentIndex + 1;
+          _render();
+        }
+      } else if (key.char == ' ') {
+        _onSelect();
+      } else if ([ControlCharacter.ctrlJ, ControlCharacter.ctrlM].contains(key.controlChar)) {
+        _onSubmit();
+      }
+    }
+
+    return _completer.future;
+  }
+
+  void _render({bool isInitialRender = false}) {
+    restoreCursorPosition();
+    clearFromCursorToEnd();
+
+    final buffer = StringBuffer();
+
+    for (final choice in _options) {
+      final index = _options.indexOf(choice);
+      final isSelected = _selectedOptions.contains(index);
+
+      buffer.writeAnsiAll([
+        if (_currentIndex == index) SetStyles(Style.foreground(Color.white)),
+        if (isSelected)
+          SetStyles(Style.foreground(Color.white))
+        else
+          SetStyles(Style.foreground(Color.brightBlack)),
+        Print(isSelected || _currentIndex == index ? '◉' : '◯'),
+        if (_currentIndex == index) SetStyles.reset,
+      ]);
+
+      if (_currentIndex == index) {
+        buffer.writeAnsi(SetStyles(Style.foreground(Color.white)));
+      } else {
+        buffer.writeAnsi(SetStyles(Style.foreground(Color.reset)));
+      }
+
+      buffer.writeAnsiAll([
+        Print(' $choice'),
+        if (_currentIndex == index) SetStyles.reset,
+        AsciiControl.lineFeed,
+      ]);
+    }
+
+    buffer.writeAnsiAll([
+      AsciiControl.lineFeed,
+      SetStyles(Style.foreground(Color.brightBlack)),
+      Print('(Type to filter, press ↑/↓ to navigate, enter to select)'),
+      SetStyles.reset,
+    ]);
+
+    if (isInitialRender) {
+      createSpace(_terminal, buffer.toString().split('\x0A').length);
+    }
+
+    stdout.write(buffer.toString());
+  }
+
+  void _onSelect() {
+    _selectedOptions.add(_currentIndex);
+    _render();
+  }
+
+  void _onSubmit() {
+    final selectedOptions = _selectedOptions.map((index) => _options[index]).toList();
+
+    restoreCursorPosition();
+    clearFromCursorToEnd();
+    showCursor();
+
+    final buffer = StringBuffer();
+    buffer.writeAnsiAll([
+      SetStyles(Style.foreground(Color.green)),
+      Print('✔'),
+      SetStyles.reset,
+      Print(' $_message '),
+      SetStyles(Style.foreground(Color.brightBlack)),
+      Print(selectedOptions
+          .map((element) => _onDisplay?.call(element) ?? element.toString())
+          .join(', ')),
+      SetStyles.reset,
+      AsciiControl.lineFeed,
+    ]);
+
+    stdout.write(buffer.toString());
+
+    _completer.complete(selectedOptions);
+    _keepAlive = false;
+  }
+}
