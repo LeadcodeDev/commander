0af500fc03cd7addc024286b72bd3781cdbe9ca9
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-11-01 09:24:22 +0100

    feat: add theme in commander cli

--- lib/src/domains/models/component_theme.dart
+++ lib/src/domains/models/component_theme.dart
@@ -2,12 +2,14 @@ import 'package:commander_ui/src/domains/themes/ask_theme.dart';
 import 'package:commander_ui/src/domains/themes/checkbox_theme.dart';
 import 'package:commander_ui/src/domains/themes/select_theme.dart';
 import 'package:commander_ui/src/domains/themes/swap_theme.dart';
+import 'package:commander_ui/src/domains/themes/task_theme.dart';
 
 final class ComponentTheme {
   final AskTheme? askTheme;
   final CheckboxTheme? checkboxTheme;
   final SwapTheme? switchTheme;
   final SelectTheme? selectTheme;
+  final TaskTheme? taskTheme;
 
-  ComponentTheme({this.askTheme, this.checkboxTheme, this.switchTheme, this.selectTheme});
+  ComponentTheme({this.askTheme, this.checkboxTheme, this.switchTheme, this.selectTheme, this.taskTheme});
 }
